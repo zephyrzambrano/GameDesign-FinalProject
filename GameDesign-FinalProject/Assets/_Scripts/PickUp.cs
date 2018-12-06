709b7e3a5312ef6bc331a2c03510b58f353d152b
zephyrzambrano <43213451+zephyrzambrano@users.noreply.github.com>
2018-12-06 12:10:40 -0500

    Imported and edited several Roll-a-Ball Assets
    - Modified PickUp prefab to make the objects smaller
    - Used the rotator script to make PickUp objects rotate
    - Modified the PlayerController script to be the PickUp script, because I used the Rigidbody First Person Controller script from Unity’s Standard Assets instead
    - Modified text objects CountText and WinText to change their location, text, and color

---
+++ GameDesign-FinalProject/Assets/_Scripts/PickUp.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour {
+
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("PickUp")) {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText() {
+        countText.text = "Points: " + count.ToString();
+        if (count >= 10) {
+            winText.text = "You Win!";
+        }
+    }
+
+}

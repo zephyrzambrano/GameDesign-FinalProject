5ed6d019799a22fbc2d2f23202e702ad11286840
zephyrzambrano <43213451+zephyrzambrano@users.noreply.github.com>
2018-12-07 11:55:12 -0500

    Created a respawn script for when the player falls off the world

---
+++ GameDesign-FinalProject/Assets/_Scripts/Respawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+    public float minHeightForDeath;
+    public Vector3 spawnPoint=new Vector3(0, 1, 0);
+    public GameObject player;
+
+    void Update() {
+        if (player.transform.position.y < minHeightForDeath) {
+            player.transform.position = spawnPoint;
+        }
+    }
+}

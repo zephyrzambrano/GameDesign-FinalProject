2b00f30376d57b528b7e063ac41e71630db50637
zephyrzambrano <43213451+zephyrzambrano@users.noreply.github.com>
2018-12-07 11:59:32 -0500

    Combined Pickup and Respawn scripts to make the Player script

--- GameDesign-FinalProject/Assets/_Scripts/Player.cs
+++ GameDesign-FinalProject/Assets/_Scripts/Player.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PickUp : MonoBehaviour {
+public class Player : MonoBehaviour {
 
     public Text countText;
     public Text winText;
 
-    private Rigidbody rb;
+    public float minHeightForDeath;
+    public Vector3 spawnPoint = new Vector3(0, 1, 0);
+    public GameObject player;
+
+    // private Rigidbody rb;
     private int count;
 
     // Use this for initialization
     void Start() {
-        rb = GetComponent<Rigidbody>();
+        // rb = GetComponent<Rigidbody>();
         count = 0;
         SetCountText();
         winText.text = "";
@@ -21,6 +25,9 @@ public class PickUp : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        if (player.transform.position.y < minHeightForDeath) {
+            player.transform.position = spawnPoint;
+        }
     }
 
     void OnTriggerEnter(Collider other) {
